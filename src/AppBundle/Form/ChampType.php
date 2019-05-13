602989f13e04c85d61db9932cb3576e5027fc3cd
higher <warhigher@gmail.com>
2019-05-13 23:13:37 +0300

    Add design for champ, change form, entity

--- src/AppBundle/Form/ChampType.php
+++ src/AppBundle/Form/ChampType.php
@@ -4,6 +4,8 @@ namespace AppBundle\Form;
 
 use AppBundle\Entity\Champ;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,10 +14,17 @@ class ChampType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('date')
-            ->add('comment')
-            ->add('editor')
-        ;
+            ->add('date', DateType::class, [
+                'widget' => 'choice',
+                'placeholder' => [
+                    'year' => 'Year', 'month' => 'Month', 'day' => 'Day',
+                ],
+                'format' => 'yyyy-MM-dd',
+            ])
+            ->add('type', ChoiceType::class, [
+                'choices' => Champ::TYPES,
+            ])
+            ->add('comment');
     }
 
     public function configureOptions(OptionsResolver $resolver)
